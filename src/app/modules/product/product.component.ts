530a7f0005a091a06505de31f7fe105a6b77dbff
midasowo <kmiduch@gmail.com>
2023-11-23 22:02:10 +0100

    add sample products

--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -11,4 +11,28 @@ import {SharedModule} from "../../shared/shared.module";
 })
 export class ProductComponent {
 
+  products = [
+    {
+      name: "Product 1",
+      category: "Category 1",
+      description: "Sample description 1",
+      price: 11.99,
+      currency: "PLN"
+    },
+    {
+      name: "Product 2",
+      category: "Category 2",
+      description: "Sample description 2",
+      price: 11.99,
+      currency: "PLN"
+    },
+    {
+      name: "Product 3",
+      category: "Category 3",
+      description: "Sample description 3",
+      price: 11.99,
+      currency: "PLN"
+    }
+  ];
+
 }
