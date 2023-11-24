670a75417a477f1f8c0284b1e2d312d3f53fca66
midasowo <kmiduch@gmail.com>
2023-11-24 18:44:08 +0100

    add product model

--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SharedModule} from "../../shared/shared.module";
+import {ProductService} from "./product.service";
+import {Product} from "./model/product";
 
 @Component({
   selector: 'app-product',
@@ -9,30 +11,19 @@ import {SharedModule} from "../../shared/shared.module";
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
 
-  products = [
-    {
-      name: "Product 1",
-      category: "Category 1",
-      description: "Sample description 1",
-      price: 11.99,
-      currency: "PLN"
-    },
-    {
-      name: "Product 2",
-      category: "Category 2",
-      description: "Sample description 2",
-      price: 11.99,
-      currency: "PLN"
-    },
-    {
-      name: "Product 3",
-      category: "Category 3",
-      description: "Sample description 3",
-      price: 11.99,
-      currency: "PLN"
-    }
-  ];
+  products: Product[] = []
 
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    this.getProducts()
+  }
+
+  getProducts() {
+    this.products = this.productService.getProducts()
+  }
+  
 }
