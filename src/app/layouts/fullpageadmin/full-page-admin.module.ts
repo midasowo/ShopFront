d69f824b3b9866aeeef07849f40515c422a5a300
midasowo <kmiduch@gmail.com>
2023-11-26 17:12:27 +0100

    styling admin panel

--- src/app/layouts/fullpageadmin/full-page-admin.module.ts
+++ src/app/layouts/fullpageadmin/full-page-admin.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from "@angular/router";
 import {SharedModule} from "../../shared/shared.module";
+import {MaterialModule} from "../../shared/material.module";
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     RouterModule,
-    SharedModule
+    SharedModule,
+    MaterialModule
   ]
 })
 export class FullPageAdminModule {
