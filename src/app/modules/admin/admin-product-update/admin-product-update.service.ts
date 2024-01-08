ecf2079eaf3d3112f6c49b237115762faf4c4f8b
midasowo <kmiduch@gmail.com>
2024-01-08 18:03:02 +0100

    add uploading image to product

--- src/app/modules/admin/admin-product-update/admin-product-update.service.ts
+++ src/app/modules/admin/admin-product-update/admin-product-update.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {AdminProductUpdate} from "./model/admin-product-update";
 import {Observable} from "rxjs";
+import {UploadResponse} from "./model/upload-response";
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,9 @@ export class AdminProductUpdateService {
 
   saveProduct(id: number, value: AdminProductUpdate) {
     return this.http.put<AdminProductUpdate>('/api/admin/products/' + id, value)
+  }
+
+  uploadImage(formData: FormData): Observable<UploadResponse> {
+    return this.http.post<UploadResponse>('/api/admin/products/upload-image', formData)
   }
 }
