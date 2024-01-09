2af22a6d3f8fe0f1778dff58ebed8db0186abb8d
midasowo <kmiduch@gmail.com>
2024-01-09 15:52:53 +0100

    added product details

---
+++ src/app/modules/product-details/product-details.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ProductDetails} from "./model/product-details";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductDetailsService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getProductDetails(slug: string): Observable<ProductDetails> {
+    return this.http.get<ProductDetails>("/api/products/" + slug)
+  }
+}
