6531451e43e1bb10aaa84152b9464855891551c4
midasowo <kmiduch@gmail.com>
2024-01-19 16:20:45 +0100

    add simple error message

--- src/app/modules/order/order.component.ts
+++ src/app/modules/order/order.component.ts
@@ -24,6 +24,7 @@ export class OrderComponent implements OnInit {
   formGroup!: FormGroup
   orderSummary!: OrderSummary
   initData!: InitData
+  errorMessage = false
 
   private statuses = new Map<string, string>([
     ["NEW", "New"]
@@ -72,9 +73,13 @@ export class OrderComponent implements OnInit {
         shipmentId: Number(this.formGroup.get('shipment')?.value.id),
         paymentId: Number(this.formGroup.get('payment')?.value.id)
       } as OrderDto)
-        .subscribe(orderSummary => {
-          this.orderSummary = orderSummary
-          this.cookieService.delete("cartId")
+        .subscribe({
+          next: orderSummary => {
+            this.orderSummary = orderSummary
+            this.cookieService.delete("cartId")
+            this.errorMessage = false
+          },
+          error: err => this.errorMessage = true
         })
     }
   }
