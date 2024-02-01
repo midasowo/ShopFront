d0dec1daf70adcd8d83070b8523027f2ade1d5a4
midasowo <kmiduch@gmail.com>
2024-02-01 12:22:33 +0100

    orders statistics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,6 +27,7 @@ import {OrderComponent} from "./modules/order/order.component";
 import {AdminOrderUpdateComponent} from "./modules/admin/admin-order/admin-order-update/admin-order-update.component";
 import {AdminOrderComponent} from "./modules/admin/admin-order/admin-order.component";
 import {AdminOrderExportComponent} from "./modules/admin/admin-order/admin-order-export/admin-order-export.component";
+import {AdminOrderStatsComponent} from "./modules/admin/admin-order/admin-order-stats/admin-order-stats.component";
 
 const routes: Routes = [
   {
@@ -63,6 +64,7 @@ const routes: Routes = [
       {path: 'admin/orders', component: AdminOrderComponent},
       {path: 'admin/orders/update/:id', component: AdminOrderUpdateComponent},
       {path: 'admin/orders/export', component: AdminOrderExportComponent},
+      {path: 'admin/orders/stats', component: AdminOrderStatsComponent},
     ]
   }
 ];
