7d3254e65ad048bf917405a0ff60df23d96303d1
midasowo <kmiduch@gmail.com>
2024-03-22 17:23:38 +0100

    added payment redirect

--- src/app/modules/order/order.component.ts
+++ src/app/modules/order/order.component.ts
@@ -81,10 +81,14 @@ export class OrderComponent implements OnInit {
       } as OrderDto)
         .subscribe({
           next: orderSummary => {
-            this.orderSummary = orderSummary
             this.cookieService.delete("cartId")
             this.errorMessage = false
             this.cartIconService.cartChanged(0)
+            if (orderSummary.redirectUrl) {
+              window.location.href = orderSummary.redirectUrl
+            } else {
+              this.orderSummary = orderSummary
+            }
           },
           error: err => this.errorMessage = true
         })
